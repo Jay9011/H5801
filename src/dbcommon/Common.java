10b92f931c61455faee2c3e3bfb62079546e1747
jiwoo-yoon <lsvn1541@naver.com>
2020-06-01 21:12:36 +0900

    지우 테스트1

--- src/dbcommon/Common.java
+++ src/dbcommon/Common.java
@@ -26,6 +26,6 @@ public class Common {
 	public static final String SQL_SELECT_ALL_BOARD = "SELECT s_uid, s_title, s_content, s_date FROM s_table WHERE m_uid = ?";
 //	내가 댓글 닷 게시글 찾기
 	public static final String SQL_SELECT_ALL_CMTBOARD = "SELECT s.s_uid, s.s_title, s.s_content, s.s_date FROM s_table s, sr_comment sc WHERE s.s_uid = sc.s_uid AND sc.m_uid = ?";
-//	내가 찜한 게시글 찾기
+//	내가 찜한 게시글 찾기기
 	public static final String SQL_SELECT_ALL_LIKEBOARD = "SELECT s.s_uid, s.s_title, s.s_content, s.s_date FROM s_favor sf, s_table s WHERE sf.s_uid = s.s_uid AND sf.m_uid = ?";
 }
