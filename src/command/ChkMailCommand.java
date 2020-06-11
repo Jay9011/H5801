ff08898eb30b7cf025ca0be86ebf2945021e4977
Jay9011 <whdtjq5904@gmail.com>
2020-06-11 18:11:44 +0900

    수정

---
+++ src/command/ChkMailCommand.java
@@ -0,0 +1,26 @@
+package command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dbcommon.DAOUser;
+
+public class ChkMailCommand implements Command {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		DAOUser udao = new DAOUser();
+
+		String email = request.getParameter("email");
+		int re = udao.check_email(email);
+
+		if(re == 1) {
+			request.setAttribute("result",re); // 이메일 있음
+		}else {
+			request.setAttribute("result", re);
+		}
+
+	}
+
+
+}
